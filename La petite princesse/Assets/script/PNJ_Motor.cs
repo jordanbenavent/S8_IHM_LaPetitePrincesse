f7b89b0fa143332eaca23135db17c340b5a75127
Jordan <jordan.benavent@etu.unice.fr>
2023-03-15 23:00:03 +0100

    PNJ labyrinthe v1

---
+++ La petite princesse/Assets/script/PNJ_Motor.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PNJ_Motor : MonoBehaviour
+{
+
+    public bool isTalking;
+    
+    public Animator animator;
+
+    // Update is called once per frame
+    void Update()
+    {
+        animator.SetBool("isTalking", isTalking);
+    }
+
+    void FixedUpdate() // Get keyboard inputs
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            isTalking=!isTalking;
+            Debug.Log("E key was pressed." + isTalking);
+        }
+    }
+}
