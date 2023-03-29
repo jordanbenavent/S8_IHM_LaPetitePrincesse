204c40fd95b453fd550661fd25f78a426267650c
BoubiaMarouane <90777897+BoubiaMarouane@users.noreply.github.com>
2023-03-29 19:22:36 +0200

    link chateau to planete culainre

--- La petite princesse/Assets/Prefab/PlanetCulinaire/Script-Culinaire/Inventory/Win.cs
+++ La petite princesse/Assets/Prefab/PlanetCulinaire/Script-Culinaire/Inventory/Win.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 using TMPro;
 
@@ -22,9 +23,9 @@ public class Win : MonoBehaviour
             StartCoroutine(DisplayTextForTwoSeconds());
         }
         //You win
-        //menuManager mM = FindObjectOfType<menuManager>();
-        //mM.giveCarTrophy();
-        //SceneManager.LoadScene(0);
+        menuManager mM = FindObjectOfType<menuManager>();
+        mM.givePizzaTrophy();
+        SceneManager.LoadScene(0);  
     }
 
     private IEnumerator DisplayTextForTwoSeconds()
